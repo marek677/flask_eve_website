d83260e052791e162b7ea1828eb57c9beef0e8e0
unknown <marek677@gmail.com>
2018-11-27 22:50:31 +0000

    Added Regional Trading summary. Changed the regional trading page

--- components/region/routes.py
+++ components/region/routes.py
@@ -5,7 +5,7 @@ import importlib
 evestatic = importlib.import_module('%s.evestatic.evestatic'%(__name__.split(".")[0]))
 import json
 import pkg_resources
-region = Blueprint("region",__name__,template_folder='templates')
+region = Blueprint("region",__name__,template_folder='templates', static_url_path="web/components/region/static")
 
 @region.app_template_filter()
 def getSysSecurityColor(value):
@@ -13,7 +13,24 @@ def getSysSecurityColor(value):
 @region.app_template_filter()
 def getM3Color(value):
     return "w3-text-red" if int(value[:-1]) > 60 else "w3-text-orange" if  int(value[:-1]) > 10 else "w3-text-green"
-	
+
+def getBestM3():
+	sd = evestatic.get_evestatic()
+	ret = []
+	for region_name in evestatic.getAllRegionNames():
+		data = json.load(open(pkg_resources.resource_filename(__name__,"static/local_dumps/%s_orders.json"%region_name.replace(" ",""))))
+		systems = list(set(map( lambda d: d["system"],data)))
+		for system in systems:
+			system_deals = sorted(filter(lambda d: d["system"] == system, data), key=lambda d: d["earnperm3"],reverse=True )
+			left_dst = 60000
+			earn_dst = 0
+			for system_deal in system_deals:
+				earn_dst = earn_dst + system_deal["earn"] if system_deal["m3"]* system_deal["volumetotal"] < left_dst else system_deal["earnperm3"] * left_dst
+				left_dst = left_dst - system_deal["m3"]* system_deal["volumetotal"]
+				if(left_dst <= 0):
+					break
+			ret.append([sd.getSystem(system_deal["system"])["name"],system_deal["jumps"], earn_dst/1000000, earn_dst/system_deal["jumps"]/1000000])
+	return ret
 def parse_stuff():
 	sd = evestatic.get_evestatic()
 	ret = []
@@ -43,8 +60,11 @@ def parse_stuff():
 		#	earnperdst*100/investment,earnperjf*100/investment,(earnperjf-155000000)*100/investment)
 		#print "\tprice:", d["price"], "volumetotal:", d["volumetotal"]
 	
-	return sorted(filter(lambda x: float(x["JF"])>155 or float(x["DST"])>x["jumps"]*2,ret), key = lambda x: x["sys_name"] )
+	return sorted(filter(lambda x: float(x["JF"])>155 or float(x["DST"])>x["jumps"]*0.5,ret), key = lambda x: x["sys_name"] )
+@region.route("/region_summary")
+def region_summary():
+	return render_template('region_summary.html',order_table=sorted(getBestM3(),key= lambda x: x[3], reverse=True))
 @region.route('/region')
 def region_main():
-	evestatic.get_evestatic()
+	getBestM3()
 	return render_template('region.html',order_table=parse_stuff())
