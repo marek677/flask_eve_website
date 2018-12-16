0031fb19c66220c4a045abae25824774c9953bd3
unknown <marek677@gmail.com>
2018-12-16 00:34:38 +0000

    Improved indy compression calc. Added menu for it

--- components/industry/routes.py
+++ components/industry/routes.py
@@ -6,6 +6,7 @@ import importlib
 evestatic = importlib.import_module('%s.evestatic.evestatic'%(__name__.split(".")[0]))
 import json
 import pkg_resources
+import datetime
 industry = Blueprint("industry",__name__,template_folder='templates', static_url_path="web/components/industry/static")
 
 def getJSON_blueprints():
@@ -19,17 +20,22 @@ def get_evestatic():
     if evestatic_db is None:
         evestatic_db = g._evestatic = StaticData()
     return evestatic_db
-def calc_compression(item_id):
-	sd = evestatic.get_evestatic()
-	data = getJSON_blueprints()
+def calc_compression(item_id,sd,minerals,data):
+	itemname = sd.getItem(item_id)["name"]
+	if not "I" in itemname.split(" ") and "II" not in itemname.split(" "):
+		return ["",0]
 	compressable_items = ["Tritanium",'Pyerite','Mexallon','Isogen','Nocxium','Zydrine','Megacyte']
-	sum_func = lambda items: sum( map( lambda item: sd.getItem(item["typeID"])["volume"] * item['quantity'], items))
-	m3_materials = sum_func(filter(lambda item:sd.getItem(item["typeID"])["name"] in compressable_items ,data[str(item_id)]["activities"]['manufacturing']["materials"]))
+	sum_func = lambda items: sum( map( lambda item: minerals.get(item["typeID"],sd.getItem(item["typeID"]))["volume"] * item['quantity'], items))
+	m3_materials = sum_func(filter(lambda item: item["typeID"] in minerals ,data[str(item_id)]["activities"]['manufacturing']["materials"]))
 	m3_products =sum_func(data[str(item_id)]["activities"]['manufacturing']["products"])
-	return [sd.getItem(item_id)["name"], m3_materials/m3_products]
+	return [itemname, m3_materials/m3_products]
 
-@industry.route('/indy_test')
-def region_main():
+@industry.route('/indy_compression')
+def indy_compression():
+	sd = evestatic.get_evestatic()
 	a = getJSON_blueprints()
-	table = map(lambda b_id: calc_compression(b_id), filter(lambda b_id: "manufacturing" in a[b_id]["activities"] and "products" in a[b_id]["activities"]["manufacturing"] and "materials" in a[b_id]["activities"]["manufacturing"] ,a))
-	return render_template('indy_test.html', my_table = sorted(table,key=lambda x:x[1],reverse=True))
+	ff = filter(lambda b_id: "manufacturing" in a[b_id]["activities"] and "products" in a[b_id]["activities"]["manufacturing"] and "materials" in a[b_id]["activities"]["manufacturing"] ,a)
+	temp = {item_id : sd.getItem(item_id) for item_id in [34,35,36,37,38,39,40] }
+	table = map(lambda b_id: calc_compression(b_id,sd,temp,a), ff)
+	lit_table = sorted(table,key=lambda x:x[1],reverse=True)[:100]
+	return render_template('indy_test.html', my_table = lit_table)
